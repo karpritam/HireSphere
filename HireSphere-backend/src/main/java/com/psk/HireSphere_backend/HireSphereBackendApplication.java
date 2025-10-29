80da717526f375e121461bb339756385fba6c96d
karpritam <karpritam194@gmail.com>
2025-10-30 00:25:25 +0530

    initial setup

---
+++ HireSphere-backend/src/main/java/com/psk/HireSphere_backend/HireSphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.psk.HireSphere_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireSphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireSphereBackendApplication.class, args);
+	}
+
+}
---
+++ HireSphere-backend/src/test/java/com/psk/HireSphere_backend/HireSphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psk.HireSphere_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireSphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
