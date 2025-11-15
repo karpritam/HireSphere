260ab165901431ce0ba63946bc73e9162a501b9b
karpritam <karpritam194@gmail.com>
2025-11-16 01:54:55 +0530

    creating jobEntity

--- HireSphere-backend/src/main/java/com/psk/HireSphere_backend/Entity/JobEntity.java
+++ HireSphere-backend/src/main/java/com/psk/HireSphere_backend/Entity/JobEntity.java
@@ -1,4 +1,46 @@
 package com.psk.HireSphere_backend.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "jobs")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String company;
+    private String location;
+
+    private String salaryRange;            // e.g. "3–5 LPA"
+    private String jobType;                // Full-time, Part-time, Remote
+    private String experienceLevel;        // Fresher, Mid-level, Senior
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+//    @PrePersist is a JPA lifecycle callback that runs automatically right before an entity is inserted (saved for the first time) into the database.
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
